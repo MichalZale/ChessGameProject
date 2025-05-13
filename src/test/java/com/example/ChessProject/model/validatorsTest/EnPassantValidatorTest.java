20392fab490f6609449b7517f9032e720ec2c17f
Michał Zalewski <michal.zalewski11.stud@pw.edu.pl>
2025-05-13 19:02:25 +0200

    Dodanie testów do obsługi promocji, więcej testów dla roszady

--- src/test/java/com/example/ChessProject/model/validatorsTest/EnPassantValidatorTest.java
+++ src/test/java/com/example/ChessProject/model/validatorsTest/EnPassantValidatorTest.java
@@ -18,7 +18,21 @@ public class EnPassantValidatorTest {
                 .build();
         GameState gs = new GameState(b, Color.WHITE,true,true,true,true,p(2,5));
         Pawn w = (Pawn)b.getPiece(p(3,4));
-        Move mv = new Move(p(3,4),p(2,5));
+        
+        Move mv = new Move(p(3,4),p(2,5), Move.MoveType.EN_PASSANT);
+        assertTrue(new PawnValidator(gs).isValidMove(mv));
+    }
+
+    @Test
+    void enPassantBlack() {
+        Board b = new BoardBuilder()
+                .add(bp(4, 4)) // Black pawn at e4
+                .add(wp(4, 3)) // White pawn at d4
+                .add(bK(0, 7)).add(wK(7, 0))
+                .build();
+        // Black to move, white pawn at (4,3) just moved from (6,3), so en passant target is (5,3) (d3)
+        GameState gs = new GameState(b, Color.BLACK, true, true, true, true, p(5, 3));
+        Move mv = new Move(p(4, 4), p(5, 3), Move.MoveType.EN_PASSANT); // Black pawn e4xd3 e.p.
         assertTrue(new PawnValidator(gs).isValidMove(mv));
     }
 
--- src/test/java/com/example/ChessProject/model/validatorsTest/PawnValidatorTest.java
+++ src/test/java/com/example/ChessProject/model/validatorsTest/PawnValidatorTest.java
@@ -3,6 +3,9 @@ package com.example.ChessProject.model.validatorsTest;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import com.example.ChessProject.model.*;
+import com.example.ChessProject.model.pieces.Knight;
+import com.example.ChessProject.model.pieces.Pawn;
+import com.example.ChessProject.model.pieces.Queen;
 import com.example.ChessProject.model.validators.PawnValidator;
 import static com.example.ChessProject.model.BoardBuilder.*;
 
@@ -31,7 +34,94 @@ class PawnValidatorTest {
                   .add( wK(7,4) ).add( bK(0,4) )
                   .build();
         GameState gs = new GameState(b,Color.WHITE,true,true,true,true,p(2,5));
-        Move mv = new Move(p(3,4),p(2,5));
+        Move mv = new Move(p(3,4),p(2,5), Move.MoveType.EN_PASSANT);
         assertTrue(new PawnValidator(gs).isValidMove(mv));
+    }
+
+    @Test
+    void whitePawnPromotesToQueen_NormalMove() {
+        Board b = new BoardBuilder()
+                .add(wp(1, 0)) 
+                .add(wK(7, 4)).add(bK(2, 4))
+                .build();
+        GameState gs = gs(b, Color.WHITE);
+        Move mv = new Move(p(1, 0), p(0, 0), new Queen(Color.WHITE, p(0,0)), Move.MoveType.PROMOTION);
+        assertTrue(new PawnValidator(gs).isValidMove(mv));
+    }
+
+    @Test
+    void blackPawnPromotesToKnight_CaptureMove() {
+        Board b = new BoardBuilder()
+                .add(bp(6, 3))
+                .add(wr(7, 2)) 
+                .add(bK(0, 4)).add(wK(5, 4))
+                .build();
+        GameState gs = gs(b, Color.BLACK);
+        Move mv = new Move(p(6, 3), p(7, 2), new Knight(Color.BLACK, p(7,2)), Move.MoveType.PROMOTION);
+        assertTrue(new PawnValidator(gs).isValidMove(mv));
+    }
+
+    @Test
+    void whitePawnPromotesToQueen_DeliversCheck() {
+        Board b = new BoardBuilder()
+                .add(wp(1, 0)) 
+                .add(wK(2, 2)) 
+                .add(bK(0, 1))
+                .build();
+        GameState gs = gs(b, Color.WHITE);
+        Move mv = new Move(p(1, 0), p(0, 0), new Queen(Color.WHITE, p(0,0)), Move.MoveType.PROMOTION);
+        assertTrue(new PawnValidator(gs).isValidMove(mv));
+
+        GameState nextGs = new PawnValidator(gs).simulateMove(mv);
+        assertTrue(new PawnValidator(nextGs).isInCheck(nextGs, Color.BLACK));
+    }
+    
+    @Test
+    void whitePawnPromotion_InvalidMoveTypeNormal() {
+        Board b = new BoardBuilder()
+                .add(wp(1, 0)) 
+                .add(wK(7, 4)).add(bK(2, 4))
+                .build();
+        GameState gs = gs(b, Color.WHITE);
+       
+        Move mv = new Move(p(1, 0), p(0, 0), Move.MoveType.NORMAL); 
+        assertFalse(new PawnValidator(gs).isValidMove(mv));
+    }
+
+    @Test
+    void whitePawnPromotion_NullPromotionPiece() {
+        Board b = new BoardBuilder()
+                .add(wp(1, 0))
+                .add(wK(7, 4)).add(bK(2, 4))
+                .build();
+        GameState gs = gs(b, Color.WHITE);
+        Move mv = new Move(p(1, 0), p(0, 0), null, Move.MoveType.PROMOTION);
+        assertFalse(new PawnValidator(gs).isValidMove(mv));
+    }
+    
+    @Test
+    void whitePawnPromotion_InvalidPromotionPieceType_ToPawn() {
+        Board b = new BoardBuilder()
+                .add(wp(1, 0))
+                .add(wK(7, 4)).add(bK(2, 4))
+                .build();
+        GameState gs = gs(b, Color.WHITE);
+
+        Move mv = new Move(p(1, 0), p(0, 0), new Pawn(Color.WHITE, p(0,0)), Move.MoveType.PROMOTION);
+        assertFalse(new PawnValidator(gs).isValidMove(mv));
+    }
+
+    @Test
+    void whitePawnPromotion_LeavesKingInCheck() {
+        Board b = new BoardBuilder()
+                .add(wp(1, 0))
+                .add(wK(1, 1))      
+                .add(br(1, 7))      
+                                   
+                .add(bK(3,3))
+                .build();
+        GameState gs = gs(b, Color.WHITE);
+        Move mv = new Move(p(1,0), p(0,0), new Queen(Color.WHITE, p(0,0)), Move.MoveType.PROMOTION); 
+        assertFalse(new PawnValidator(gs).isValidMove(mv));
     }
 }
